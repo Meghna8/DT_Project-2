57c1f9c88c2026a8daeb75e4852687a15681dcb4
Meghna8 <35065059+Meghna8@users.noreply.github.com>
2018-04-02 15:33:28 +0530

    completed blog and forum

---
+++ ChatBackend/src/main/java/com/niit/Config/DBConfig.java
@@ -0,0 +1,84 @@
+package com.niit.Config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.niit.DAO.BlogDAO;
+import com.niit.DAO.BlogDAOImpl;
+//import com.niit.DAO.ForumDAO;
+//import com.niit.DAO.ForumDAOImpl;
+//import com.niit.DAO.JobDAO;
+//import com.niit.DAO.JobDAOImpl;
+//import com.niit.DAO.UserDAO;
+//import com.niit.DAO.UserDAOImpl;
+//import com.niit.model.ApplyJob;
+import com.niit.model.Blog;
+import com.niit.model.BlogComment;
+//import com.niit.model.Forum;
+//import com.niit.model.ForumComment;
+//import com.niit.model.Friends;
+//import com.niit.model.Job;
+//import com.niit.model.UserDetails;
+
+@Configuration
+@EnableTransactionManagement
+
+public class DBConfig {
+	@Bean
+	public DataSource getH2DataSource()
+	{   
+		DriverManagerDataSource dataSource=new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/LetsChat");
+		dataSource.setUsername("deepika");
+		dataSource.setPassword("deepika");
+		
+		System.out.println("---Data Source Created---");
+		return dataSource;
+	}
+	
+	@Bean
+	public SessionFactory sessionFactory() 
+	{
+		LocalSessionFactoryBuilder factoryBuilder = new LocalSessionFactoryBuilder(getH2DataSource());
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		hibernateProperties.setProperty("hibernate.format_sql", "true");
+		hibernateProperties.setProperty("hibernate.use_sql_commnets", "true");
+		factoryBuilder.addProperties(hibernateProperties);
+		
+		factoryBuilder.addAnnotatedClass(Blog.class);
+		factoryBuilder.addAnnotatedClass(BlogComment.class);
+		
+		System.out.println("Creating SessionFactory Bean");
+		return factoryBuilder.buildSessionFactory();
+
+	}
+	
+	@Bean(name="blogDAO")
+	public BlogDAO getBlogDAO()
+	{
+		System.out.println("----blog DAO Implementation---");
+		return new BlogDAOImpl();
+	}
+	
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory)
+	{
+		System.out.println("---Transaction Manager----");
+		return new HibernateTransactionManager(sessionFactory);
+	}
+
+}
---
+++ ChatBackend/src/main/java/com/niit/DAO/ForumDAOImpl.java
@@ -0,0 +1,75 @@
+package com.niit.DAO;
+
+import java.util.List;
+
+import com.niit.model.Forum;
+import com.niit.model.ForumComment;
+
+public class ForumDAOImpl implements ForumDAO {
+
+	public boolean addForum(Forum forum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteForum(Forum forum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateForum(Forum forum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<Forum> listForums(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean approveForum(Forum forum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean rejectForum(Forum forum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Forum getForum(int forumId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Forum> listAllForums() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean incrementLikes(Forum forum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addForumComment(ForumComment forumComment) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteForumComment(ForumComment forumComment) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ForumComment getForumComment(int commentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<ForumComment> listForumComments(int forumid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
